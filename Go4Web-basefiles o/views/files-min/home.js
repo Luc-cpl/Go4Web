45f20b492e8969a99dd557c5f9a929b86b3906c4
LUCAS CARVALHO PEREIRA LISO <lucas.carvalhopl@outlook.com>
2017-06-20 17:37:20 -0300

    delete some files

--- Go4Web-basefiles o/views/files-min/home.js
+++
@@ -1,1 +0,0 @@
-var Greeting=React.createClass({displayName:"Greeting",render:function(){return React.createElement("p",null,"Hello, Universe")}});ReactDOM.render(React.createElement(Greeting,null),document.getElementById("greeting-div"));
